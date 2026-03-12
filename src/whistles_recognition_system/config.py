59bded7600ba043da09806871b030e1ea7717263
ddietor <ddietor@icm.csic.es>
2026-03-12 20:51:25 +0100

    Update config.py

--- src/whistles_recognition_system/config.py
+++ src/whistles_recognition_system/config.py
@@ -1,13 +1,3 @@
-
-# %% NOT PUBLIC DATA:
-NFFT = 2048
-fs = 195312.5/2 # [Hz]
-overlap_bin = 0.5
-Tpx = NFFT*(1-overlap_bin)/fs
-Fpx = fs/NFFT
-Fpx_0 = 1955    # Frequency of the bottom pixel in the spectrogram [Hz]
-Npxs = 608      # Total number of pixels in spectrogram height
-
 # %% Parameters used in the spectrogram creation (image files):
 Fpx = 62.5      # Frequency resolution of the spectrogram [Hz]
 Tpx = 8e-3      # Time resolution of the spectrogram [s]
